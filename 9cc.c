11d9604b83e003b1690556555cf3f011041ad1a0
ahirose <ahirose@gmail.com>
2019-08-17 17:54:17 +0900

    updated error messages

--- 9cc.c
+++ 9cc.c
@@ -26,14 +26,30 @@ struct Token
 //現在着目しているトークン
 Token *token;
 
+//入力プログラム
+char *user_input;
+
 //エラーを報告するための関数
 //printfと同じ引数をとる
+//void error(char *fmt, ...){
+//	va_list ap;
+//	va_start(ap, fmt);
+//	vfprintf(stderr, fmt, ap);
+//	fprintf(stderr, "\n");
+//	exit(1);
+//}
 
-void error(char *fmt, ...){
+//エラー箇所を報告する
+void error_at(char *loc, char *fmt, ...){
 	va_list ap;
-	va_start(ap, fmt);
-	vfprintf(stderr, fmt, ap);
-	fprintf(stderr, "\n");
+	va_start(ap,fmt);
+
+	int pos=loc - user_input;
+	fprintf(stderr,"%s\n",user_input);
+	fprintf(stderr,"%*s",pos,""); //pos個の空白を出力
+	fprintf(stderr,"^ ");
+	vfprintf(stderr,fmt,ap);
+	fprintf(stderr,"\n");
 	exit(1);
 }
 
@@ -51,7 +67,7 @@ bool consume(char op){
 // そうでないときはエラー。
 void expect(char op){
 	if(token->kind!=TK_RESERVED|| token->str[0]!=op)
-		error("not a '%c'",op);
+		error_at(token->str, "not a '%c'",op);
 	token=token->next;
 
 }
@@ -60,7 +76,7 @@ void expect(char op){
 // そうでないときはエラー。
 int expect_number(){
 	if(token->kind!=TK_NUM)
-		error("'not a number");
+		error_at(token->str,"not a number");
 	int val = token->val;
 	token=token->next;
 	return val;
@@ -92,7 +108,7 @@ Token *tokenize(char *p){
 			continue;
 		}
 
-		if(*p=='+'|| *p=='-'){
+		if(*p=='+' || *p=='-'){
 			cur = new_token(TK_RESERVED, cur, p++);
 			continue;
 		}
@@ -102,22 +118,22 @@ Token *tokenize(char *p){
 			cur->val = strtol(p,&p,10);
 			continue;
 		}
-		error("tokenize error '%c'",*p);
+		error_at(p,"tokenize error\n");
 	}
 	new_token(TK_EOF,cur,p);
 	return head.next;
 }
 
 
-
 int main(int argc, char **argv)
 {
 	if (argc != 2)
 	{
-		error("you need two parameters\n");
+		error_at(token->str,"you need two parameters\n");
 		return 1;
 	}
 
+	user_input=argv[1];
 	//トークナイズする
 	token=tokenize(argv[1]);
 
--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ try() {
 	input="$2"
 
 	./9cc "$input" > tmp.s
-	gcc -o tmp tmp.s
+	gcc -o tmp tmp.s 
 	./tmp
 	actual="$?"
 
@@ -18,7 +18,7 @@ try() {
 
 try 0 0
 try 42 42
-try 21 "5 + 20 - 4"
-try 41 " 12 + 34 - 5 "
+try 21 " 5 + 20 - 4"
+try 41 " 12 + 34 - foo "
 echo OK
 
